fc5d9d8fb8367d86eb783765857afb4dde1b0b0b
Lucas Kauit <lucas@onway.com.br>
2020-12-13 20:07:38 -0300

    created cart state and mutations

---
+++ src/store/modules/cart/index.js
@@ -0,0 +1,50 @@
+export default {
+
+    state: {
+        products: []
+    },
+
+    mutations: {
+        ADD_PRODUCT(state, product) {
+            state.products.push({
+                quantity: 1,
+                productUuid: product.uuid,
+                product,
+            })
+        },
+
+        REMOVE_PROUDCT(state, product) {
+            state.products = state.products.filter((cartProduct, index) => {
+                return cartProduct.uuid !== product.uuid
+            })
+        },
+
+
+        INCREMENT_QUANTITY(state, product) {
+            state.products = state.products.map((cartProduct, index) => {
+                if(cartProduct.uuid === product.uuid){
+                    state.products[index].quantity++
+                }
+
+                return state.products[index]
+            })
+        },
+
+        DECREMENT_QUANTITY(state, product) {
+            state.products = state.products.filter((cartProduct, index) => {
+                if(cartProduct.uuid === product.uuid){
+                    state.products[index].quantity--
+                }
+
+                if(state.products[index].quantity > 0){
+                    return state.products[index]
+                }
+            })
+        },
+
+        CLEAR_CART(state) {
+            state.products = []
+        }
+    },
+
+}
